2267a3365ef0cfc83f2b95b055414f29d2617a4d
YAGYANK CHADHA <yagyank@gmail.com>
2017-09-02 17:10:29 +0800

    Dashboard creation

---
+++ CryptoCompare/FirstViewController.swift
@@ -0,0 +1,33 @@
+//
+//  FirstViewController.swift
+//  CryptoCompare
+//
+//  Created by YAGYANK CHADHA on 1/9/17.
+//  Copyright © 2017 YAGYANK CHADHA. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    
+    //Mark: Properties
+
+ 
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+   
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ DashboardTableView.swift
@@ -0,0 +1,21 @@
+//
+//  DashboardTableView.swift
+//  CryptoCompare
+//
+//  Created by YAGYANK CHADHA on 2/9/17.
+//  Copyright © 2017 YAGYANK CHADHA. All rights reserved.
+//
+
+import UIKit
+
+class DashboardTableView: UITableView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
